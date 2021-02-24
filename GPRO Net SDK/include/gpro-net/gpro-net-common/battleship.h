74f126ec627b843e5ffa1aab3c46189cf3c808ea
Stephen <stephen.hill@mymail.champlain.edu>
2021-02-24 00:56:55 -0500

    Forgot to mention that separate header and cpp for battleship functions works now. Put some more comments for functions

--- GPRO Net SDK/include/gpro-net/gpro-net-common/battleship.h
+++ GPRO Net SDK/include/gpro-net/gpro-net-common/battleship.h
@@ -9,57 +9,36 @@
 #include <string>
 #include <iostream>
 
-		/*
-		void PrintBoards(gpro_battleship board1, gpro_battleship board2, int playerIndex);
-
-	//takes in input(something like B4) and translates into the correct spot in gpro_battleship, input will be in correct format
-		Vec2 ProcessInput(std::string input);
-
-	//check if input is within board bounds
-		bool IsInputValid(std::string input);
-
-		bool isSpaceOpen(gpro_battleship& boardToUse, int direction, Vec2 startLocation, int length);
-
-		void PopulateBoardWithShip(gpro_battleship& boardToUse, int direction, Vec2 startLocation, int length, gpro_battleship_flag shipType);
-
-	//0 is left, 1 is up, 2 is right, 3 is down
-		bool CheckAndCreateShip(gpro_battleship& boardToUse, int direction, Vec2 startLocation);
-	//sets up a single ship, rerun with all types of ships
-		void SetupShip(gpro_battleship& board1, gpro_battleship& board2, int playerIndex, gpro_battleship_flag shipType);
-
-		void SetupBoard(gpro_battleship& board1, gpro_battleship& board2, int playerIndex);
-		*/
-
 		
-		void PrintBoards(gpro_battleship board1, gpro_battleship board2, int playerIndex);
+void PrintBoards(gpro_battleship board1, gpro_battleship board2, int playerIndex);
 		
-		//takes in input(something like B4) and translates into the correct spot in gpro_battleship, input will be in correct format
-		Vec2 ProcessInput(std::string input);
+//takes in input(something like B4) and translates into the correct spot in gpro_battleship, input will be in correct format
+Vec2 ProcessInput(std::string input);
 		
 
-		//check if input is within board bounds
-		bool IsInputValid(std::string input);
+//check if input is within board bounds
+bool IsInputValid(std::string input);
 		
 
-
-		bool isSpaceOpen(gpro_battleship& boardToUse, int direction, Vec2 startLocation, int length);
+//Checks if there is space open in the given direction(within board bounds and not going through a ship)
+bool isSpaceOpen(gpro_battleship& boardToUse, int direction, Vec2 startLocation, int length);
+		
+//Fills board with the specified ship in the direction
+void PopulateBoardWithShip(gpro_battleship& boardToUse, int direction, Vec2 startLocation, int length, gpro_battleship_flag shipType);
 		
 
-		void PopulateBoardWithShip(gpro_battleship& boardToUse, int direction, Vec2 startLocation, int length, gpro_battleship_flag shipType);
+//0 is left, 1 is up, 2 is right, 3 is down
+bool CheckAndCreateShip(gpro_battleship& boardToUse, int direction, Vec2 startLocation);
 		
-
-		//0 is left, 1 is up, 2 is right, 3 is down
-		bool CheckAndCreateShip(gpro_battleship& boardToUse, int direction, Vec2 startLocation);
+//sets up a single ship, rerun with all types of ships
+void SetupShip(gpro_battleship& board1, gpro_battleship& board2, int playerIndex, gpro_battleship_flag shipType);
 		
-		//sets up a single ship, rerun with all types of ships
-		void SetupShip(gpro_battleship& board1, gpro_battleship& board2, int playerIndex, gpro_battleship_flag shipType);
-		
-
-		void SetupBoard(gpro_battleship& board1, gpro_battleship& board2, int playerIndex);
+//Do the setup phase for the player
+void SetupBoard(gpro_battleship& board1, gpro_battleship& board2, int playerIndex);
 
 
-		//Do the attack phase for the player
-		void AttackPhase(gpro_battleship& board1, gpro_battleship& board2, int playerIndex);
+//Do the attack phase for the player
+void AttackPhase(gpro_battleship& board1, gpro_battleship& board2, int playerIndex);
 		
 		
 
